278635b9ce20c37f63bf79c423fbec6d020d1921
amorobg <amorobg@gmail.com>
2024-12-18 12:06:32 +0000

    Modify the hero text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,14 +6,13 @@ const Hero = ({ home }) => {
       {/* <h1>Africa Climate Solution & Sustainable Agriculture Network</h1> */}
       <p className="text-lg">
         Fragmented efforts, duplicated resources, and limited knowledge sharing
-        impede progress and hinder effective action in combating climate change
+        impede progress in combating climate change in Africa
       </p>
 
       <p>
-        ACSSAN provides the platform for all stakeholders in climate action and
-        sustainable agriculture to coordinate, collaborate, and combine efforts
-        to adequately tackle the menace of climate change while producing enough
-        food to feed the continent
+        ACSSAN provides the platform for all stakeholders in climate action to
+        collaborate and combine efforts to adequately combat climate change,
+        while producing enough food to feed the continent
       </p>
       <button>Join the Network</button>
     </section>
